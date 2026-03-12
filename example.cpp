f473210d2ed64f6588c9ede84f30cdfb98915090
trmaa <trmaayt@gmail.com>
2026-03-12 10:12:01 +0100

    bfs

--- example.cpp
+++ example.cpp
@@ -5,13 +5,18 @@
 using namespace std;
 
 int main() {
-	cout << "G: Graf K10\n";
-	Graf::LlistaAdj G(Graf::Tipus::K, 10);
+	cout << "G: Graf C10\n";
+	Graf::LlistaAdj G(Graf::Tipus::C, 10);
 
 	cout << "Mida: " << G.Mida() << endl;
 
 	cout << "DFS:\n";
 	Graf::DFS(G, 0, [](int v) {
+		cout << v << endl;
+	});
+
+	cout << "BFS:\n";
+	Graf::BFS(G, 0, [](int v) {
 		cout << v << endl;
 	});
 }
--- graf/arlogithm.hpp
+++ graf/arlogithm.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <queue>
 #include "type.hpp"
 
 using namespace std;
@@ -19,9 +20,32 @@ namespace Graf {
 		DFS_impl(G, v, seen, cb);
 	}
 
+	static void BFS_impl(const LlistaAdj& G, int v, vector<bool>& seen, void (*visit)(int)) {
+                queue<int> q;
+                
+                visit(v);
+                seen[v] = true;
+                q.push(v);
+
+                while (!q.empty()) {
+                        int current = q.front();
+                        q.pop();
+
+                        for (int u : G.at(current))
+                                if (!seen[u]) {
+                                        visit(u);
+                                        seen[u] = true;
+                                        q.push(u);
+                                }
+                }
+        }
+
+        inline void BFS(const LlistaAdj& G, int v, void (*cb)(int)) {
+                vector<bool> seen(G.Ordre());
+                BFS_impl(G, v, seen, cb);
+        }	
+
 	inline int grau(const LlistaAdj& G, int v) {
 		return G.at(v).size();
 	}
-
-	// TODO BFS
 };
